4f07688f16d9db8dd9bd885d021cd88d84fad3bc
Guilherme Rezende <guilhermebr@gmail.com>
2025-08-14 19:23:34 -0300

    feat: use mailvault sdk at cli

--- app/cli/config.go
+++ app/cli/config.go
@@ -8,10 +8,10 @@ import (
 )
 
 type Config struct {
-	ServerURL    string `json:"server_url"`
-	AccessToken  string `json:"access_token,omitempty"`
-	UserEmail    string `json:"user_email,omitempty"`
-	UserID       string `json:"user_id,omitempty"`
+	ServerURL   string `json:"server_url"`
+	AccessToken string `json:"access_token,omitempty"`
+	UserEmail   string `json:"user_email,omitempty"`
+	UserID      string `json:"user_id,omitempty"`
 }
 
 func getConfigPath() string {
@@ -20,53 +20,45 @@ func getConfigPath() string {
 
 func loadConfig() (*Config, error) {
 	configPath := getConfigPath()
-	
+
 	// If config file doesn't exist, return default config
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		return &Config{
 			ServerURL: serverURL,
 		}, nil
 	}
-	
+
 	data, err := os.ReadFile(configPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read config file: %w", err)
 	}
-	
+
 	var config Config
 	if err := json.Unmarshal(data, &config); err != nil {
 		return nil, fmt.Errorf("failed to parse config file: %w", err)
 	}
-	
+
 	// Override with command line flags if provided
 	if serverURL != "http://localhost:8080" {
 		config.ServerURL = serverURL
 	}
-	
+
 	return &config, nil
 }
 
 func saveConfig(config *Config) error {
 	configPath := getConfigPath()
-	
+
 	data, err := json.MarshalIndent(config, "", "  ")
 	if err != nil {
 		return fmt.Errorf("failed to marshal config: %w", err)
 	}
-	
+
 	if err := os.WriteFile(configPath, data, 0600); err != nil {
 		return fmt.Errorf("failed to write config file: %w", err)
 	}
-	
-	return nil
-}
 
-func isLoggedIn() bool {
-	config, err := loadConfig()
-	if err != nil {
-		return false
-	}
-	return config.AccessToken != ""
+	return nil
 }
 
 func requireAuth() (*Config, error) {
@@ -74,10 +66,10 @@ func requireAuth() (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	if config.AccessToken == "" {
 		return nil, fmt.Errorf("not logged in. Please run 'mailsafe login' first")
 	}
-	
+
 	return config, nil
 }
